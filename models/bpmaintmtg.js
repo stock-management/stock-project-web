b77b41c173f229586df40797b80569edcc7688ee
黃仕鈞 <huangshijun@MacBook-Pro.localdomain>
2018-02-11 22:27:03 -0600

    Revert "demo"
    This reverts commit 08f837926deffe66310a2ec355ead516837aae97.

--- models/bpmaintmtg.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var bpmaintmtgSchema = require('../schemas/bpmaintmtg.js'); //引入'../schemas/bpmaintmtg.js'导出的模式模块
-
-// 编译生成bpmaintmtg模型
-var bpmaintmtg = mongoose.model('bpmaintmtg', bpmaintmtgSchema);
-
-// 将bpmaintmtg模型[构造函数]导出
-module.exports = bpmaintmtg;
--- public/js/paginator.js
+++
@@ -1,9 +0,0 @@
-/*---------------分页选中判断开始------------*/
- var curPage = $( "#curP" ).val();
- $( ".pagination a" ).each( function(){
-     if ($( this ).text() == curPage ) {
-         $( this ).parent().addClass( 'active' )
-             .siblings( "li" ).removeClass( "active" );
-     }
- } );
-/*---------------分页选中判断结束------------*/
